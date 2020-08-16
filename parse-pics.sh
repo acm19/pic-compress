d656b5793d2f4b56500fddc4fbac0b871d28107d
acm <acm19@users.noreply.github.com>
2020-08-16 13:20:12 +0200

    Add support to multi directoy cards
    Adds support to multi directoy pictures. Nikon adds a new directory for
    every 1000 pictures this scripts handles that and moves the videos to a
    different directory.

--- parse-pics.sh
+++ parse-pics.sh
@@ -20,15 +20,31 @@ source_dir=${1%/}
 target_dir=${2%/}
 tmp_target=$target_dir/tmp_image
 
-mkdir $tmp_target
-jpegoptim -m$RATE -p -d $tmp_target $source_dir/*.JPG
+mkdir "$tmp_target"
 
-for i in $tmp_target/*.JPG; do
-  pic_date=`stat -c %y $i | awk '{print $1}'`
-  timestamp_without_timezone=`eval date -d '$pic_date' +%s`
-  dir_name=`LC_ALL=en_US.UTF-8; printf "%(%Y %m %B %d)T\n" $timestamp_without_timezone`
+for i in $source_dir/*/; do
+  prefix=$(echo "$i" | awk -F'/' '{print $(NF-1)}')
+  find $i -type f -name *.MOV -o -name *.JPG \
+   | awk -v prefix=$prefix -v dest=$tmp_target -F'/' '{system("cp -a " $0 " " dest "/" prefix "-" $NF)}'
+done
+
+jpegoptim -m$RATE -p $tmp_target/*
+
+for i in $tmp_target/*; do
+  pic_date=$(stat -c %y $i | awk '{print $1}')
+  timestamp_without_timezone=$(eval date -d '$pic_date' +%s)
+  dir_name=$(LC_ALL=en_US.UTF-8; printf "%(%Y %m %B %d)T\n" $timestamp_without_timezone)
   mkdir -p "$target_dir/$dir_name"
   mv "$i" "$target_dir/$dir_name"
 done
 
 rm -rf $tmp_target
+
+for i in $target_dir/*/; do
+  find "$i" -name *.MOV | grep . > /dev/null && mkdir -p "$i"videos && mv "$i"*.MOV "$i"videos
+  pics_name=$(echo "$i" | awk -F'/' '{print $2}' | awk '{print $1 "_" $2 "_" $3 "_" $4'})
+  n=1; for f in "$i"*.JPG; do mv "$f" "$(printf "%s%s_%05i.jpg" "$i" "$pics_name" "$n")"; ((n++)); done
+done
+
+echo "Files in original directory $(find $source_dir -type f | wc -l)"
+echo "Files in compressed directory $(find $target_dir -type f | wc -l)"
